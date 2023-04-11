6c384f8d52e9a0bac81d4b4a2f0984cb80dc8be9
uiratan <uiratan@gmail.com>
2023-04-10 21:53:55 -0300

    model - adicionado Acesso

---
+++ src/main/java/br/com/uira/lojavirtual/model/Acesso.java
@@ -0,0 +1,33 @@
+package br.com.uira.lojavirtual.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Acesso implements GrantedAuthority {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable=false)
+	private String descricao; 
+
+	@Override
+	public String getAuthority() {
+		return this.descricao;
+	}
+
+}
