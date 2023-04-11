2617930daa9e86ea0e41cfe782f319199bf380b1
uiratan <uiratan@gmail.com>
2023-04-10 21:35:25 -0300

    model - adicionado MarcaProduto

--- src/main/java/br/com/uira/lojavirtual/model/MarcaProduto.java
+++ src/main/java/br/com/uira/lojavirtual/model/MarcaProduto.java
@@ -6,32 +6,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+@Data
+//@Table(name = "marca_produto")
+//@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
 public class MarcaProduto {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String nomeDesc;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNomeDesc() {
-		return nomeDesc;
-	}
-
-	public void setNomeDesc(String nomeDesc) {
-		this.nomeDesc = nomeDesc;
-	}
+	private String descricao;
 
 }
